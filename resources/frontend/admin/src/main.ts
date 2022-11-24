f2bfa77b94225ddaf5eb06ea446a1b8f61ac585f
LEENTechDev <wapp.visuals@gmail.com>
2022-11-24 11:55:21 +0800

    Update main.ts

--- resources/frontend/admin/src/main.ts
+++ resources/frontend/admin/src/main.ts
@@ -6,9 +6,13 @@ import router from './router'
 
 import './assets/main.css'
 
+import lang from './helpers/lang';
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.config.globalProperties.$lang = lang;
